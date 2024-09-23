181711e44a9ce71d56a922fa136562d66b32d0a7
saif mohamed alashry <105813935+saifalashry@users.noreply.github.com>
2024-09-24 02:56:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="/home/saif/project intern 1/styles.css"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
